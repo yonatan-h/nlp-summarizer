4439dc1d060ec0e862ef45c2a2c22ee113f1c694
yonatan-h <yon019hl@gmail.com>
2025-01-12 14:58:03 +0300

    mvp

--- main.py
+++ main.py
@@ -1,4 +1,70 @@
+import spacy
+from collections import Counter, defaultdict
+from heapq import nlargest
+from spacy.lang.en.stop_words import STOP_WORDS as stop_words
+from string import punctuation
 
 with open("text.txt", "r") as file:
     text = file.read()
-print(text)
+# print(text)
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(text)
+
+def sanitize(text):
+    sanitized = []
+    punctuations = set(list(punctuation+'\n'))
+
+    for letter in text:
+        if letter not in punctuations:
+            sanitized.append(letter)
+
+    return "".join(sanitized)
+
+def get_tokens(doc, sanitize):
+    tokens = []
+    for token in doc:
+        text = token.text.lower().strip()
+        if text in stop_words: continue
+        sanitized = sanitize(text)
+        if sanitized: tokens.append(sanitized)
+
+    return tokens
+
+#get tokens
+tokens = get_tokens(doc, sanitize)
+
+#get frequecies
+def get_normalized_freq(tokens):
+    counts = Counter(tokens)
+    freqs = defaultdict(lambda: 0.0)
+    max_freq = max(counts.values())
+
+    for token in counts:
+        freqs[token] = counts[token]/max_freq
+
+    return freqs
+
+freqs = get_normalized_freq(tokens)
+
+#get sentence scores
+def get_sentence_scores(doc, freqs, sanitize):
+    sentence_scores = defaultdict(lambda: 0.0)
+
+    for sentence in doc.sents:
+        for token in sentence:
+            text = sanitize(token.text)
+            sentence_scores[sentence.text] += freqs[text]
+
+    return sentence_scores
+
+sentence_scores = get_sentence_scores(doc, freqs, sanitize)
+
+def get_summary(sentence_scores, n):
+    summary = nlargest(n, sentence_scores, key=sentence_scores.get)
+    return "".join(summary)
+
+summary = get_summary(sentence_scores, 3)
+print(summary)
+
+
+
