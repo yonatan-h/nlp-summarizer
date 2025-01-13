76843af37dd31e6ee86af28a0d8017726b234dfc
yonatan-h <yon019hl@gmail.com>
2025-01-13 11:57:14 +0300

    fix port

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
 from summarize import summarize_bp 
+import os
 
 app = Flask(__name__)
 app.register_blueprint(summarize_bp)
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    print('my Running on port', port, flush=True)
+    app.run(port=port, host="0.0.0.0")
