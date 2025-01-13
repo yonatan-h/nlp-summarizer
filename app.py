3c191d0fba136309689dc46d5ea7a8449c37f510
yonatan-h <yon019hl@gmail.com>
2025-01-13 12:21:14 +0300

    install gunicorn

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(summarize_bp)
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    print('my Running on port', port, flush=True)
-    app.run(port=port, host="0.0.0.0")
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     print('my Running on port', port, flush=True)
+#     app.run(port=port, host="0.0.0.0")
